f3389e5167401b6b5a9c74f510b89330b9a28626
Rerisson Daniel <rerissondaniel@gmail.com>
2015-12-01 17:59:59 -0300

    ForCodeDataException added.

---
+++ ForCode_COMMONS/src/br/edu/commons/forcode/exceptions/ForCodeDataException.java
@@ -0,0 +1,15 @@
+package br.edu.commons.forcode.exceptions;
+
+public class ForCodeDataException extends Exception {
+
+	/**
+	 *v0.1
+	 */
+	private static final long serialVersionUID = 2794113774245425609L;
+	
+	public ForCodeDataException(String message){
+		super(message);
+	}
+	
+	
+}
