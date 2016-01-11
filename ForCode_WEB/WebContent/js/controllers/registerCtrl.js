f98f847caaa2bfd37433e95a4913bc0d4a4b72e3
José Renan <joserenansl99@gmail.com>
2016-01-10 23:45:19 -0300

    Logging and creating user now

--- ForCode_WEB/WebContent/js/controllers/registerCtrl.js
+++ ForCode_WEB/WebContent/js/controllers/registerCtrl.js
@@ -7,16 +7,6 @@ angular.module("forCode").controller("registerCtrl", function($scope, $location,
 			return false;
 	};
 
-	$scope.errorMessage = function(field, message){
-
-		console.log(field.$name);
-
-		if(field.$error.required)
-			return "";
-		else
-			return typeof(message) === "undefined"? "Inválido" : message;
-	};
-
 	$scope.institutions = institutions.data;
 
 	$scope.doRegister = function(user){
