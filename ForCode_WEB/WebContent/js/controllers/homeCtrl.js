998ee3bf7648a53b7022e6b8c5a931b417eb2cbf
José Renan <joserenansl99@gmail.com>
2016-02-01 01:35:00 -0300

    Some tests things are added

---
+++ ForCode_WEB/WebContent/js/controllers/homeCtrl.js
@@ -0,0 +1,26 @@
+angular.module("forCodeAdmin").controller("homeCtrl", function($scope, config, userService, $cookies){
+
+	$scope.user = $cookies.getObject('user');
+	
+	$scope.isContestant = function (){
+		if ($scope.user.typeUser === "CONTESTANT")
+			return true;
+		else
+			return false
+	};
+
+	$scope.isAdmin = function (){
+		if ($scope.user.typeUser === "ADMIN")
+			return true;
+		else
+			return false
+	};
+
+	$scope.isManager = function (){
+		if ($scope.user.typeUser === "MANAGER")
+			return true;
+		else
+			return false
+	};
+
+});
--- ForCode_WEB/WebContent/js/controllers/testeCtrl.js
+++
@@ -1,5 +0,0 @@
-angular.module("forCodeAdmin").controller("testeCtrl", function($scope, config, userService, $cookies){
-
-	$scope.teste = $cookies.getObject('user');
-	
-});
